defd54a68bdbebd6b27aa297851e49be0b7582a7
Tobibur <tobiburrahman786@gmail.com>
2018-06-27 18:51:28 +0530

    Added Animation

--- app/src/main/java/com/example/tobibur/journalapp/MainActivity.java
+++ app/src/main/java/com/example/tobibur/journalapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.tobibur.journalapp;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,12 +16,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tobibur.journalapp.adapter.RecyclerAdapter;
 import com.example.tobibur.journalapp.helper.DatabaseHandler;
 import com.example.tobibur.journalapp.model.Journal;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
 import com.jetradar.desertplaceholder.DesertPlaceholder;
 
 import java.util.List;
@@ -36,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     Integer dbPostCount;
+    LayoutAnimationController mController=null;
+
+    private static final String PREFS_NAME = "prefs";
+    private static final String IS_FIRST_LAUNCH = "is_first";
+
 
 
     @Override
@@ -66,9 +77,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                startActivity(new Intent(getApplicationContext(),PostActivity.class));
+                addNewPost();
             }
         });
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean isFirst = settings.getBoolean(IS_FIRST_LAUNCH, true);
+        if (isFirst){
+            settings.edit().putBoolean(IS_FIRST_LAUNCH, false).apply();
+            showTips();
+        }
+    }
+
+    private void showTips() {
+        TapTargetView.showFor(this,                 // `this` is an Activity
+                TapTarget.forView(findViewById(R.id.fab), "Add a new post!", "Tap this button for new post")
+                        // All options below are optional
+                        .outerCircleColor(R.color.colorAccent)      // Specify a color for the outer circle
+                        .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
+                        .targetCircleColor(R.color.colorWhite)   // Specify a color for the target circle
+                        .titleTextSize(20)                  // Specify the size (in sp) of the title text
+                        .titleTextColor(R.color.colorWhite)      // Specify the color of the title text
+                        .descriptionTextSize(10)            // Specify the size (in sp) of the description text
+                        .descriptionTextColor(R.color.colorAccent)  // Specify the color of the description text
+                        .textColor(R.color.colorPrimary)            // Specify a color for both the title and description text
+                        .textTypeface(Typeface.SANS_SERIF)  // Specify a typeface for the text
+                        .dimColor(R.color.colorBlack)            // If set, will dim behind the view with 30% opacity of the given color
+                        .drawShadow(true)                   // Whether to draw a drop shadow or not
+                        .cancelable(true)                  // Whether tapping outside the outer circle dismisses the view
+                        .tintTarget(true)                   // Whether to tint the target view's color
+                        .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
+                        .icon(getDrawable(R.drawable.ic_add_black_24dp))                     // Specify a custom drawable to draw as the target
+                        .targetRadius(60),                  // Specify the target radius (in dp)
+                new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
+                    @Override
+                    public void onTargetClick(TapTargetView view) {
+                        super.onTargetClick(view);      // This call is optional
+                        addNewPost();
+                    }
+                });
+    }
+
+    private void addNewPost() {
+        startActivity(new Intent(getApplicationContext(),PostActivity.class));
     }
 
     @Override
@@ -112,11 +163,14 @@ public class MainActivity extends AppCompatActivity {
             textView.setVisibility(View.VISIBLE);
         }
 
+        mController = AnimationUtils.loadLayoutAnimation(this,R.anim.layout_fall_down);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
 
         recyclerView.setHasFixedSize(true);
         RecyclerAdapter adapter = new RecyclerAdapter(posts, this);
+        recyclerView.setLayoutAnimation(mController);
         adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
     }
